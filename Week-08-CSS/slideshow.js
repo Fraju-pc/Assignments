78f8edce70bc40d822dd0a58ed3221282754b58f
Fraju-pc <patrick.f.corcoran@gmail.com>
2023-05-26 09:11:00 -0400

    Final Version with Comments

--- Week-08-CSS/slideshow.js
+++ Week-08-CSS/slideshow.js
@@ -1,16 +1,21 @@
+//Slideshow Script
+//Variable and Function Call
 let index = 0;
 carousel();
 
+//Carousel Function
 function carousel() {
   let i;
   let x = document.getElementsByClassName("slides");
   for (i = 0; i < x.length; i++) {
+    //hide the images not currently displayed
     x[i].style.display = "none";  
   }
   index++;
   if (index > x.length) {
     index = 1
-  }    
+  }
+  //display image    
   x[index-1].style.display = "block";  
   setTimeout(carousel, 4000); 
 }
