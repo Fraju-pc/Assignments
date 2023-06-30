f59fca5615847050f054e870fa6c2c37c982ef97
Fraju-pc <patrick.f.corcoran@gmail.com>
2023-06-30 11:52:29 -0400

    .

--- Week-13-Intro_to_React_Webpack_Components_and_JSX/src/navins.jsx
+++ Week-13-Intro_to_React_Webpack_Components_and_JSX/src/navins.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import NavBar from './NavBar.jsx'
 import "../src/app.css"
 
+//script to insert nav bar into multiple pages
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
